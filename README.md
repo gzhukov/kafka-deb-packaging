6b8aa86b2ed0c1330f3456e2b6b65d5773750d23
root <root@kafka1d.gce-eu.kafka.iponweb.net>
2015-03-20 17:48:56 +0000

    Fixed kafka package

--- README.md
+++ README.md
@@ -3,6 +3,20 @@ kafka-deb-packaging
 
 Simple debian packaging for Apache Kafka
 
+## Install
+1) Install sbt
+http://www.scala-sbt.org/release/tutorial/Installing-sbt-on-Linux.html
+
+2) Install fpm
+apt-get install ruby-dev gcc make
+gem install fpm
+
+3) Run script
+bash build_kafka.sh
+
+
 ## Changelog
+
+2015-Mar-20 : Some needed IOW fixes
 2015-Mar-18 : Updated for latest Kafka 0.8.2.1 and use sbt in system path.
 
--- build_kafka.sh
+++ build_kafka.sh
@@ -4,19 +4,20 @@ set -e
 set -u
 name=kafka
 version=0.8.2.1
+scala_version=2.10
 description="Apache Kafka is a distributed publish-subscribe messaging system."
 url="https://kafka.apache.org/"
 arch="all"
 section="misc"
 license="Apache Software License 2.0"
 package_version="-1"
-src_package="kafka-${version}-src.tgz"
-download_url="http://mirror.sdunix.com/apache/kafka/${version}/${src_package}"
+bin_package="kafka_${scala_version}-${version}.tgz"
+download_url="http://apache-mirror.rbc.ru/pub/apache/kafka/${version}/${bin_package}"
 origdir="$(pwd)"
 
 #_ MAIN _#
 rm -rf ${name}*.deb
-if [[ ! -f "${src_package}" ]]; then
+if [[ ! -f "${bin_package}" ]]; then
   wget ${download_url}
 fi
 mkdir -p tmp && pushd tmp
@@ -24,19 +25,11 @@ rm -rf kafka
 mkdir -p kafka
 cd kafka
 mkdir -p build/usr/lib/kafka
-mkdir -p build/etc/default
-mkdir -p build/etc/init
-mkdir -p build/etc/kafka
-mkdir -p build/var/log/kafka
+cp -r ${origdir}/files/etc build/etc
+cp -r ${origdir}/files/var build/var
+cp -r ${origdir}/files/deb ./deb
 
-cp ${origdir}/kafka-broker.default build/etc/default/kafka-broker
-cp ${origdir}/kafka-broker.upstart.conf build/etc/init/kafka-broker.conf
-
-tar zxf ${origdir}/${src_package}
-cd kafka-${version}-src
-sbt update
-sbt package
-mv config/log4j.properties config/server.properties ../build/etc/kafka
+tar zxf ${origdir}/${bin_package} && cd kafka_${scala_version}-${version}
 mv * ../build/usr/lib/kafka
 cd ../build
 
@@ -49,7 +42,10 @@ fpm -t deb \
     --category ${section} \
     --vendor "" \
     --license "${license}" \
-    -m "${USER}@localhost" \
+    -m "Gleb Zhukov <gzhukov@iponweb.net>" \
+    --after-install "../deb/postinst" \
+    --after-remove "../deb/postrm" \
+    --before-remove "../deb/prerm" \
     --prefix=/ \
     -s dir \
     -- .
