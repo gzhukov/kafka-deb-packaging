fd947271ec70c9d0d8edf2064903785c83872646
root <root@admin1b.gce-eu.kafka.iponweb.net>
2015-05-06 14:01:34 +0000

    Fixed some settings

--- build.sh
+++ build.sh
@@ -10,15 +10,17 @@ url="https://kafka.apache.org/"
 arch="all"
 section="misc"
 license="Apache Software License 2.0"
-package_version="-1"
+package_version=$1
 bin_package="kafka_${scala_version}-${version}.tgz"
 download_url="http://apache-mirror.rbc.ru/pub/apache/kafka/${version}/${bin_package}"
+log4j_extras_url="http://central.maven.org/maven2/log4j/apache-log4j-extras/1.2.17/apache-log4j-extras-1.2.17.jar"
 origdir="$(pwd)"
 
 #_ MAIN _#
 rm -rf ${name}*.deb
 if [[ ! -f "${bin_package}" ]]; then
   wget ${download_url}
+  wget ${log4j_extras_url}
 fi
 mkdir -p tmp && pushd tmp
 rm -rf kafka
@@ -26,16 +28,16 @@ mkdir -p kafka
 cd kafka
 mkdir -p build/usr/lib/kafka
 cp -r ${origdir}/files/etc build/etc
-cp -r ${origdir}/files/var build/var
 cp -r ${origdir}/files/deb ./deb
 
 tar zxf ${origdir}/${bin_package} && cd kafka_${scala_version}-${version}
 mv * ../build/usr/lib/kafka
+mv ${origdir}/apache-log4j-extras*.jar ../build/usr/lib/kafka/libs/
 cd ../build
 
 fpm -t deb \
     -n ${name} \
-    -v ${version}${package_version} \
+    -v ${version}-${package_version} \
     --description "${description}" \
     --url="{$url}" \
     -a ${arch} \
